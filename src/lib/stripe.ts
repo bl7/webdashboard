cacb6a64381d972e4b28f69b15185f63bcaa1eb8
biswash <madridistabiswash@gmail.com>
2025-06-14 13:44:35 +0545

    khoi k khoi k

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,9 @@
 import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  typescript: true,
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY is not defined")
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16" as any,
 })
