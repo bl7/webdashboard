d44e65fd610fa8e2670da5075a4bf7ff647a42eb
biswash <madridistabiswash@gmail.com>
2025-07-01 12:09:50 +0545

    vercel insight

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "@/components/dashboard/sidebar"
 import { PrinterProvider } from "@/context/PrinterContext"
 import PrinterStatusBar from "@/components/PrinterStatusBar"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "Instalabel ",
@@ -17,6 +18,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
           <div className="space-y-6 p-6">{children}</div>
         </div>
       </PrinterProvider>
+      <SpeedInsights />
     </main>
   )
 }
