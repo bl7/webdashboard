bff9567b81f176c1bbfa5e405a82b7046c7aa0ee
biswash <madridistabiswash@gmail.com>
2025-08-20 22:04:34 +0545

    for seo aaaaaaaaaaaaaaahhhhhh

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,20 +1,20 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
+      userAgent: "*",
+      allow: "/",
       disallow: [
-        '/dashboard/',
-        '/boss/',
-        '/bossdashboard/',
-        '/api/',
-        '/_next/',
-        '/admin/',
-        '/private/',
+        "/dashboard/",
+        "/boss/",
+        "/bossdashboard/",
+        "/api/",
+        "/_next/",
+        "/admin/",
+        "/private/",
       ],
     },
-    sitemap: 'https://instalabel.co/sitemap.xml',
+    sitemap: "https://www.instalabel.co/sitemap.xml",
   }
-} 
+}
