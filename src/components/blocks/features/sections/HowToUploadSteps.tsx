155d3fb6b85aca3dd2f03f775b752efe464d496e
biswash <madridistabiswash@gmail.com>
2025-09-27 21:53:33 +0545

    embed

--- src/components/blocks/features/sections/HowToUploadSteps.tsx
+++ src/components/blocks/features/sections/HowToUploadSteps.tsx
@@ -52,7 +52,7 @@ export const HowToUploadSteps = () => (
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/watch?v=NPSIOvzwJ-s"
+        src="https://www.youtube.com/embed/NPSIOvzwJ-s"
         title="How to Use InstaLabel Bulk Upload"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
